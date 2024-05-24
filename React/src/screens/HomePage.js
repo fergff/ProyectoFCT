8ecba1e616470a4446b8b148c377414e2b38522f
fergff <alumno.102809@ies-azarquiel.es>
2024-05-24 11:25:10 +0200

    [Dev] Teminado Register y inicado el Home

--- React/src/screens/HomePage.js
+++ React/src/screens/HomePage.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ref, onValue, getDatabase } from 'firebase/database';
 
-function HomePage() {
+function HomeScreen() {
+  const [devices, setDevices] = useState([]);
+  const [userId, setUserId] = useState('');
+  const [selectedDevice, setSelectedDevice] = useState(null);
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const storedUserId = localStorage.getItem('userId');
+        if (storedUserId) {
+          setUserId(storedUserId);
+          const devicesRef = ref(getDatabase(), `usuarios/${storedUserId}/devices`);
+          onValue(devicesRef, (snapshot) => {
+            const devicesData = snapshot.val();
+            const devicesList = devicesData ? Object.keys(devicesData).map(key => ({
+              id: key,
+              ...devicesData[key]
+            })) : [];
+            setDevices(devicesList);
+          });
+        }
+      } catch (error) {
+        console.log("Error retrieving userId", error);
+      }
+    };
+
+    fetchUserId();
+  }, []);
+
+
   return (
-    <div className="container mt-5">
-      <h1>¡Bienvenido a la Aplicación!</h1>
-      <p>Pagina de Inicio</p>
+    <div>
+      <div className='bg-green d-flex p-1 justify-content-between align-items-center shadow'>
+        <img src={require('../assets/Icons/icon.png')} width={70}  height={70} />
+        <div className='me-3'>
+          <h3 className='text-light'>Cuenta</h3>
+        </div>
+      </div>
+      <div className='container mt-2'>
+      <div className=' d-flex px-1 justify-content-between align-items-end'>
+        <h1 className='text-green fw-bold fst-italic'>Dispositivos</h1>
+        <p className='fs-4 fst-italic text-secondary'>Usuario: {userId}</p> 
+      </div>
+
+      <hr className='text-green' />
+      <div className='row align-items-stretch'>
+        {devices.map((item) => (
+
+          <div className='col-6 col-md-4 col-lg-3'>
+              <div className='card h-100 w-100 bg-green shadow' key={item.id} >
+              <div className='d-flex justify-content-between align-items-center rounded  p-3 '>
+                <img
+                  src={require('../assets/Icons/PottedPlant.png')}
+                  width={100}
+                  height={100}
+                />
+                <div className='text-light fs-5 '>{item.id}</div>
+              </div>
+            </div>
+          </div>
+          
+        ))}
+      </div>
     </div>
+    </div>
+    
   );
 }
 
-export default HomePage;
+export default HomeScreen;
+
--- React/src/screens/RegisterScreen.js
+++ React/src/screens/RegisterScreen.js
@@ -1,33 +1,81 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ref, set, get, query, orderByChild, equalTo } from 'firebase/database';
+import { database } from '../conexion/firebaseConfig';
 
 function RegisterScreen() {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
+
+  const handleCancel = () => {
+    navigate('/');
+  };
+
   const handleRegister = async () => {
-    if (password !== confirmPassword) {
-      alert('Las contraseñas no coinciden');
+    if (!email || !name || !password || !confirmPassword) {
+      setError('Por favor, rellena todos los campos.');
       return;
     }
-    console.log('Registrando usuario:', name);
-    navigate('/home'); 
+    if (password !== confirmPassword) {
+      setError('Las contraseñas no coinciden');
+      return;
+    }
+
+    // Verifica si el nombre de usuario ya existe
+    const usersRef = ref(database, 'usuarios/');
+    const usersQuery = query(usersRef, orderByChild('name'), equalTo(name));
+    const snapshot = await get(usersQuery);
+    if (snapshot.exists()) {
+      setError('El nombre de usuario ya está en uso. Elige otro nombre.');
+      return;
+    }
+    const userId = generateShortId(); // Generar ID corto
+    // Si el nombre de usuario no existe, procede a crear el nuevo usuario
+    const newUserRef = ref(database, `usuarios/${userId}`); // Use Date.now() para un ID simple
+    set(newUserRef, {
+      email: email,
+      name: name,
+      pass: password,
+    }).then(() => {
+      setError('');
+      navigate('/'); // Navegar a la página login después del registro
+    }).catch((error) => {
+      setError('Error al crear el usuario: ' + error.message);
+    });
+  };
+
+  const generateShortId = () => {
+    const timestamp = Date.now().toString();
+    const shortTimestamp = timestamp.substring(timestamp.length - 6); // Obtener los últimos 6 dígitos del timestamp
+    const randomNum = Math.floor(Math.random() * 100).toString().padStart(2, '0'); // Número aleatorio entre 0 y 99
+    return `user${shortTimestamp}${randomNum}`; // Concatenar para formar el ID
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Registrarse</h2>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" className="form-control" />
-      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Nombre" className="form-control mt-3" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Contraseña" className="form-control mt-3" />
-      <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirmar Contraseña" className="form-control mt-3" />
-      <button onClick={handleRegister} className="btn btn-primary mt-3">Registrarse</button>
+    <div className='d-flex flex-column justify-content-center' style={{ height: '100vh' }}>
+       <div className="container d-flex justify-content-center">
+          <div className='w-50 p-4 border-green-rounded'>
+            <h2 className='text-green'>Registrarse</h2>
+
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" className="login-input"/>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Nombre" className="login-input" />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Contraseña" className="login-input"/>
+            <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirmar Contraseña"  className="login-input"/>
+
+            <div className='d-flex justify-content-between'>
+              <button onClick={handleRegister} className="login-button mx-2">Registrarse</button>
+              <button onClick={handleCancel} className="login-button mx-2">Cancelar</button>
+            </div>
+            {error && <div className='mt-2 ms-2' style={{ color: 'red' }}>{error}</div>}
+          </div>
+       </div>
     </div>
   );
 }
 
 export default RegisterScreen;
-
